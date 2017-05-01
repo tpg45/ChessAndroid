be97ccf5a7018b8cfe4a6e0620a8478b2fe78841
nprez <nick_prezioso@hotmail.com>
2017-05-01 17:03:14 -0400

    display pieces correctly; refactored some code in MainActivity

---
+++ app/src/main/java/com/example/tim/chess/ImageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.tim.chess;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return 64;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(95, 95));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(0, 0, 0, 0);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        //imageView.setImageResource(mThumbIds[position]);
+        //img.setImageResource(getImg(board[i][j].toString()));
+
+        int x = position%8;
+        int y = 7-(position/8);
+        imageView.setImageResource(MainActivity.getImg(MainActivity.board[y][x].toString()));
+
+        return imageView;
+    }
+}
